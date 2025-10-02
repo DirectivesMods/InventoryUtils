eede57228c4996f9018b795ce878dc0da89f0ab8
Directives <87586485+DirectivesMods@users.noreply.github.com>
2025-10-02 16:29:01 -0700

    initial commit

---
+++ src/main/java/guiclicker/Reference.java
@@ -0,0 +1,8 @@
+package guiclicker;
+
+public class Reference
+{
+    public static final String MOD_ID = "guiclicker";
+    public static final String MOD_NAME = "GUI Clicker";
+    public static final String MOD_VERSION = "@MOD_VERSION@";
+}
---
+++ src/main/java/guiclicker/proxy/CommonProxy.java
@@ -0,0 +1,6 @@
+package guiclicker.proxy;
+
+public class CommonProxy
+{
+    public void registerEventHandlers() { }
+}
